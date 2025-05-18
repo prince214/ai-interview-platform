d99413bfc05ce5864f2deafbe8ed1735cffa18af
prince.paraste <princeparaste78@gmail.com>
2025-05-18 15:30:21 +0530

    ignore eslint and typescript warnings/error for testing

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDEqBpbjSHZC_1gSfI_Nx8HEOL-YpTsn_I",
-  authDomain: "ai-interview-platform-a067d.firebaseapp.com",
-  projectId: "ai-interview-platform-a067d",
-  storageBucket: "ai-interview-platform-a067d.firebasestorage.app",
-  messagingSenderId: "589901652616",
-  appId: "1:589901652616:web:ea72808209e3f2aa81f53d",
-  measurementId: "G-QXDWK07DXD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
